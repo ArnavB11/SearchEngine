f7e704a7337c562121b56760ff388d745bc2796e
arnav_balodi <balodiarnav@gmail.com>
2026-03-17 20:23:30 +0530

    Initial commit

---
+++ search-exe/backend/server.js
@@ -0,0 +1,1 @@
+const express = require('express'); const cors = require('cors'); const app = express(); app.use(cors()); app.get('/api/search', (req, res) => { res.json({ results: ['Result 1 for ' + req.query.q] }); }); app.listen(5000, () => console.log('Backend running on port 5000'));
